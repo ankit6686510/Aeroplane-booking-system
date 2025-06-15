350df6619d763bc7d54523f514219e9dba10508f
Ankit Jha <ankit6686510@gmail.com>
2025-06-15 12:31:34 +0530

    starting new projects

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express =  require('express');
+const {PORT} =  require('./config');
+
+const apiRoutes =  require('./routes');
+
+const app =  express();
+
+app.use('/api' ,  apiRoutes);
+
+app.listen(PORT ,  () =>{
+    console.log(`server is running on port ${PORT}`);
+    
+})
